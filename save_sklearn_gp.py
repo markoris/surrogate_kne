0580b11d339e9bf86d1c2fb42730f8077473abb3
Marko Ristic <mr3364@rit.edu>
2021-03-26 07:52:42 -0400

    added predict function with capability of lum -> mag conversion

--- save_sklearn_gp.py
+++ save_sklearn_gp.py
@@ -13,6 +13,7 @@ import numpy as np
 
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, WhiteKernel, ConstantKernel as C
+from scipy.linalg import cholesky, cho_solve
 
 def gp2json(gp):
     """
@@ -73,6 +74,43 @@ def load_gp(fname_base):
     gp.alpha_ = my_alpha
     gp._y_train_std = float(my_json['y_train_std'])
     gp._y_train_mean = float(my_json['y_train_mean'])
+    
+    gp.predict = predict
+    
     return gp
+
+def predict(model, inputs, output=''):
+    
+    K = model.kernel_(model.X_train_) # setting up the kernel
+    K[np.diag_indices_from(K)] += model.alpha
+    model.L_ = cholesky(K, lower=True) # recalculating L matrix since this is what makes the pickled models bulky
+    model._K_inv = None # has to be set to None so the GP knows to re-calculate matrices used for uncertainty
+    K_trans = model.kernel_(inputs, model.X_train_)
+    pred = K_trans.dot(model.alpha_)
+    pred = model._y_train_std * pred + model._y_train_mean
+    v = cho_solve((model.L_, True), K_trans.T)
+    y_cov = model.kernel_(inputs) - K_trans.dot(v)
+    err = np.sqrt(np.diag(y_cov))
+    
+    def lums_to_mags(data): # add err argument that handles conversion of errors
+        
+        r = 3.086e18 # parsec to cm
+        r *= 10 # 10 pc for absolute magnitude
+        flux = data / (4 * np.pi * r**2)
+        flux[np.where(flux <= 0)] = 1e-50 # eliminate zeroes with really small values
+        mags = -48.6 - 2.5 * np.log10(flux)
+        return mags
+        
+    if output == 'mags':
+        return lums_to_mags(pred), lums_to_mags(err) # fix error propagation, currently incorrect
+    
+    if output == 'ABmags':
+        wavs = np.array([476., 621., 754., 900., 1020., 1220., 1630., 2190.])
+        ab_offsets = np.array([-0.103, 0.146, 0.366, 0.528, 0.634, 0.938, 1.379, 1.900]) # Hewett et al. 2006, MNRAS, 367, 454-468 Table 7
+        offset_idx = np.argmin(np.abs([[inputs[i, -1]-wavs[j] for j in range(wavs.shape[0])] for i in range(inputs.shape[0])]), axis=1)
+        # determines offset value by finding closest matching filter to user-specified wavelength
+        return lums_to_mags(pred)+ab_offsets[offset_idx], lums_to_mags(err)
+    
+    return pred, err
 
                             
